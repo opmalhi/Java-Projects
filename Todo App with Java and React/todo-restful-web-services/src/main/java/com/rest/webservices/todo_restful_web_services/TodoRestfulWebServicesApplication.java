89cbdd95cd32951136b4e94df237071a3c860cb3
Om Parkash <opmalhi@gmail.com>
2024-11-06 17:26:12 -0600

    Initial commit

---
+++ Todo App with Java and React/todo-restful-web-services/src/main/java/com/rest/webservices/todo_restful_web_services/TodoRestfulWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.rest.webservices.todo_restful_web_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoRestfulWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoRestfulWebServicesApplication.class, args);
+	}
+
+}
---
+++ Todo App with Java and React/todo-restful-web-services/src/test/java/com/rest/webservices/todo_restful_web_services/TodoRestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.webservices.todo_restful_web_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoRestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
