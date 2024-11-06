b85b4470a9a685b1740b09c25c5881996689dca9
Om Parkash <opmalhi@gmail.com>
2024-11-06 17:58:10 -0600

    Enabling CORS Requests for Spring Boot REST API

--- Todo App with Java and React/todo-app/src/components/todo/WelcomeComponent.jsx
+++ Todo App with Java and React/todo-app/src/components/todo/WelcomeComponent.jsx
@@ -1,14 +1,37 @@
 import { useParams, Link } from "react-router-dom"
+import axios from "axios"
 
 const WelcomeComponent = () => {
 
     const { username } = useParams()
+
+    const callHelloWorldRestApi = async () => {
+
+        await axios.get('http://localhost:8080/hello-world')
+            .then((response) => successfulResponse(response))
+            .catch((error) => errorResponse(error))
+            .finally(() => console.log("clean up"))
+
+    }
+
+    const successfulResponse = (response) => {
+        console.log(response)
+    }
+
+    const errorResponse = (error) => {
+        console.log(error)
+    }
 
     return (
         <div>
             <h1>Welcome {username}</h1>
             <div>
                 Manage your todos - <Link to="/todos">Go here</Link>
+            </div>
+            <div>
+                <button className="btn btn-success m-5" onClick={callHelloWorldRestApi}>
+                    Call Hello World
+                </button>
             </div>
         </div>
     )
--- Todo App with Java and React/todo-restful-web-services/src/main/java/com/rest/webservices/todo_restful_web_services/TodoRestfulWebServicesApplication.java
+++ Todo App with Java and React/todo-restful-web-services/src/main/java/com/rest/webservices/todo_restful_web_services/TodoRestfulWebServicesApplication.java
@@ -2,12 +2,34 @@ package com.rest.webservices.todo_restful_web_services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TodoRestfulWebServicesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoRestfulWebServicesApplication.class, args);
+	}
+
+	//http://localhost:3000/ to 8080
+	//Cross Origin Requests
+	//Allow all requests only from http://localhost:3000/
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				// addMapping for specific path here we are allowing all path
+				// allowedMethods where we can define specific methods like GET,POST,PATCH,DELETE and * means all methods
+				// allowedOrigins where we define our outside address from where we will access or send request to methods
+				registry.addMapping("/**")
+						.allowedMethods("*")
+						.allowedOrigins("http://localhost:3000");
+			}
+		};
 	}
 
 }
